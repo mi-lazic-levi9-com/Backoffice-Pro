0d67a1f9edb7a9171efcd37989dabed6f82a96f0
Miljana Tukic <m.tukic@nl.worldstream.com>
2026-02-12 11:00:46 +0100

    error message for deleting user is more specific, variable name changing

--- backoffice-pro/src/app/users/users.service.ts
+++ backoffice-pro/src/app/users/users.service.ts
@@ -18,8 +18,8 @@ export interface IUser {
 })
 export class UsersService {
   private httpClient = inject(HttpClient);
-  private users = signal<IUser[]>([]);
-  loadedUsers = this.users.asReadonly();
+  private usersSignal = signal<IUser[]>([]);
+  loadedUsers = this.usersSignal.asReadonly();
 
   loadUsers() {
     return this.fetchUsers('http://localhost:3000/users');
@@ -30,23 +30,28 @@ export class UsersService {
       map((resData) => {
         return resData.users;
       }),
-      tap((users) => this.users.set(users)),
+      tap((users) => this.usersSignal.set(users)),
     );
   }
 
   removeUser(userId: string) {
-    const prevUsers = this.users();
+    const prevUsers = this.usersSignal();
 
     //optimistic update
     if (prevUsers.some((u) => u.id === userId)) {
-      this.users.set(prevUsers.filter((u) => u.id !== userId));
+      this.usersSignal.set(prevUsers.filter((u) => u.id !== userId));
     }
 
     return this.httpClient.delete(`http://localhost:3000/users/${userId}`).pipe(
       catchError((error) => {
         //because of optimistic update
-        this.users.set(prevUsers);
-        return throwError(() => new Error('something went wrong when deleting user'));
+        this.usersSignal.set(prevUsers);
+        return throwError(
+          () =>
+            new Error(
+              `Failed to delete user ${userId}: ${error?.message ?? 'Unknown error occurred'}`,
+            ),
+        );
       }),
     );
   }
